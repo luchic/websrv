f503246de4984a9a7818445e48a5ab5c98de515c
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-01-10 14:11:48 +0100

    Add to HttpServer class connection storage

--- reaserch/includes/HttpServer.hpp
+++ reaserch/includes/HttpServer.hpp
@@ -1,18 +1,27 @@
 #ifndef __HTTPSERVER_HPP
 #define __HTTPSERVER_HPP
 
+#include "IHttpConnection.hpp"
 #include "IHttpServer.hpp"
 #include <sys/epoll.h>
+#include <map>
 
-class HttpServer : IHttpServer
+class HttpServer : public IHttpServer
 {
 private:
 	int _listenFd;
 	int _epfd;
+	
+	// std::unordered_map just for remember
+	// For me it make no sence without threads
+	std::map<int, IHttpConnection*> _connections;
 
 	void _setup();
 	void _setNonBlocking(int fd);
 	int _setupSocket();
+	void _initialConnection();
+	void _handleInited(int fd);
+	void _handleEpollQue(struct epoll_event *que, int size);
 
 public:
 	HttpServer();
